c852dac33fb79578f0f1f62af5f734c99f2d8502
unknown <tarpel.gta@gmail.com>
2025-07-07 04:18:42 +0300

    chore: install husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  return (
-    <h1>Wookiee Library Project</h1>
-  )
+  return <h1>Wookiee Library Project</h1>;
 }
 
-export default App
+export default App;
