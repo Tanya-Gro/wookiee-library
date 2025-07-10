dca92dc6d1e0db1af79a881dcd278c5b92ec98f0
unknown <tarpel.gta@gmail.com>
2025-07-10 20:23:40 +0300

    refactor: app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,65 @@
 import { Component, type ReactNode } from 'react';
 import './App.css';
 import SearchForm from './components/SearchForm';
+import CardForm from './components/CardForm';
+import MyButton from './components/UI/button/MyButton';
+import Pagination from './components/Pagination';
 
-class App extends Component {
+type AppState = {
+  searchQuery: string;
+  currentPage: number;
+  countPages: number;
+  isLoading: boolean;
+  hasError: boolean;
+};
+
+class App extends Component<object, AppState> {
   state = {
     searchQuery: '',
+    currentPage: 1,
+    countPages: 1,
+    isLoading: false,
+    hasError: false,
   };
 
   handleSearchChange = (newValue: string): void => {
     this.setState({ searchQuery: newValue });
   };
 
+  handlePageChange = (newPage: number): void => {
+    this.setState({ currentPage: newPage });
+  };
+
+  updatePagesCount = (newPage: number): void => {
+    this.setState({ countPages: newPage });
+  };
+
   render(): ReactNode {
     return (
-      <SearchForm
-        searchQuery={this.state.searchQuery}
-        onSearchQueryChange={this.handleSearchChange}
-      />
+      <>
+        <SearchForm
+          searchQuery={this.state.searchQuery}
+          onSearchQueryChange={this.handleSearchChange}
+          updatePagesCount={this.updatePagesCount}
+        />
+        <CardForm
+          searchQuery={this.state.searchQuery}
+          currentPage={this.state.currentPage}
+          updatePagesCount={this.updatePagesCount}
+        />
+        <hr />
+        <Pagination
+          currentPage={this.state.currentPage}
+          countPages={this.state.countPages}
+          onPageChange={this.handlePageChange}
+        />
+        <hr />
+        <div className="wrapper right">
+          <MyButton onClick={() => console.error('error')}>
+            Error Button
+          </MyButton>
+        </div>
+      </>
     );
   }
 }
