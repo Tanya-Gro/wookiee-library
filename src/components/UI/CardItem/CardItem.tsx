8becc33237ad2263bd29bdc6e12d0153ac0b267b
unknown <tarpel.gta@gmail.com>
2025-07-25 00:01:30 +0300

    refactor: CardItem into FC

--- src/components/UI/CardItem/CardItem.tsx
+++ src/components/UI/CardItem/CardItem.tsx
@@ -1,28 +1,29 @@
 import type { Card } from '../../../app/types';
+import type { FC, ReactNode } from 'react';
 
-import { Component, type ReactNode } from 'react';
 import styles from './CardItem.module.css';
 
 type CardItemProps = {
   card: Card;
 };
 
-export default class CardItem extends Component<CardItemProps> {
-  render(): ReactNode {
-    const { imageURL, name, birth_year, homeworld } = this.props.card;
-    return (
-      <div className={styles.card} key={name} data-testid="card">
-        <img src={imageURL} alt={name} className={styles.img} />
-        <div>
-          <h3 className={styles.title}>{name}</h3>
-          <p className={styles.info} data-testid="birth-year">
-            Birth year: {birth_year}
-          </p>
-          <p className={styles.info} data-testid="home-world">
-            Home world: {homeworld}
-          </p>
-        </div>
+const CardItem: FC<CardItemProps> = ({
+  card: { imageURL, name, birth_year, homeworld },
+}): ReactNode => {
+  return (
+    <div className={styles.card} key={name} data-testid="card">
+      <img src={imageURL} alt={name} className={styles.img} />
+      <div>
+        <h3 className={styles.title}>{name}</h3>
+        <p className={styles.info} data-testid="birth-year">
+          Birth year: {birth_year}
+        </p>
+        <p className={styles.info} data-testid="home-world">
+          Home world: {homeworld}
+        </p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default CardItem;
