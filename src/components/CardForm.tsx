0a5fc7498cf7cbf2dfa6ebd555cbbe4085046711
unknown <tarpel.gta@gmail.com>
2025-07-25 00:13:12 +0300

    refactor: CardForm into FC

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -1,29 +1,25 @@
 import type { Card } from '../app/types';
+import type { FC, ReactNode } from 'react';
 
-import { Component, type ReactNode } from 'react';
 import CardItem from './UI/CardItem/CardItem';
 
 type CardFormProps = {
   cards: Card[];
 };
 
-class CardForm extends Component<CardFormProps> {
-  render(): ReactNode {
-    const { cards } = this.props;
-
-    return (
-      <>
-        <hr />
-        <div className="card-list wrapper" data-testid="card-form">
-          {cards.length ? (
-            cards.map((card) => <CardItem card={card} key={card.id} />)
-          ) : (
-            <p className="info-message">Nothing found 😭</p>
-          )}
-        </div>
-      </>
-    );
-  }
-}
+const CardForm: FC<CardFormProps> = ({ cards }): ReactNode => {
+  return (
+    <>
+      <hr />
+      <div className="card-list wrapper" data-testid="card-form">
+        {cards.length ? (
+          cards.map((card) => <CardItem card={card} key={card.id} />)
+        ) : (
+          <p className="info-message">Nothing found 😭</p>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default CardForm;
