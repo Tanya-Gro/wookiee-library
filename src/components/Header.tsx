5a88342d5e72e9a16af030f870fd05b0e0d981d9
unknown <tarpel.gta@gmail.com>
2025-07-26 17:03:43 +0300

    feat: add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import type { FC } from 'react';
+
+import { NavLink } from 'react-router-dom';
+
+const Header: FC = () => {
+  return (
+    <>
+      <header className="wrapper center">
+        <nav>
+          <NavLink to="/home" className="link">
+            Home
+          </NavLink>
+          <NavLink to="/about" className="link">
+            About
+          </NavLink>
+        </nav>
+      </header>
+      <hr />
+    </>
+  );
+};
+
+export default Header;
