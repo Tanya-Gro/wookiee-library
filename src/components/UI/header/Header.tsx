5f9a004ad7cb0e34c925f2bda7ddf25d6e18542b
unknown <tarpel.gta@gmail.com>
2025-07-27 21:16:09 +0300

    style: move header

--- src/components/UI/header/Header.tsx
+++ src/components/UI/header/Header.tsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 const Header: FC = () => {
   return (
     <>
-      <header className="wrapper center">
+      <header className="wrapper center header">
         <nav>
           <NavLink to="/home" className="link">
             Home
