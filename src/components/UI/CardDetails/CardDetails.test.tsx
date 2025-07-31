7a4bd3f2cce9db6bc4e88b1fc7e2143dc02fd536
unknown <tarpel.gta@gmail.com>
2025-07-31 16:45:17 +0300

    test: add card details

---
+++ src/components/UI/CardDetails/CardDetails.test.tsx
@@ -0,0 +1,55 @@
+import CardDetails from './CardDetails';
+import { render, screen, waitFor } from '@testing-library/react';
+import { mockDetails } from '../../../test-utils/mocks/details';
+import { MemoryRouter } from 'react-router-dom';
+import type { Details } from '../../../app/types';
+
+vi.mock('../../../api/getDetails', () => ({
+  default: async (): Promise<Details> => mockDetails[0],
+}));
+
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useSearchParams: (): URLSearchParams[] => {
+      const searchParams = new URLSearchParams({ page: '1', details: '1' });
+      return [searchParams];
+    },
+    useNavigate: (): void => {
+      vi.fn();
+    },
+  };
+});
+
+describe('CardDetails', () => {
+  it('should render detail card', async () => {
+    render(
+      <MemoryRouter>
+        <CardDetails />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+
+    await waitFor((): void => {
+      expect(screen.getByText(/Details:/i)).toBeInTheDocument();
+      expect(screen.getByText(mockDetails[0].name)).toBeInTheDocument();
+
+      if (
+        mockDetails[0].formerAffiliations &&
+        mockDetails[0].formerAffiliations.length > 0
+      ) {
+        expect(
+          screen.getByText(
+            /Former Affiliations: {mockDetails[0].formerAffiliations[0]}/i
+          )
+        ).toBeInTheDocument();
+      }
+
+      expect(
+        screen.getByRole('button', { name: /close/i })
+      ).toBeInTheDocument();
+    });
+  });
+});
