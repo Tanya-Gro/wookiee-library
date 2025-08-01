206401e130efa4a33138c5c121281dda5a789b6c
unknown <tarpel.gta@gmail.com>
2025-08-01 16:37:16 +0300

    refactor: isFetchData add conditions

--- src/helpers/isFetchError.ts
+++ src/helpers/isFetchError.ts
@@ -3,5 +3,5 @@ import type { FetchError, DataType, Details } from '../app/types';
 export function isFetchError(
   data: DataType | FetchError | Details
 ): data is FetchError {
-  return 'message' in data;
+  return 'hasError' in data && data.hasError && 'message' in data;
 }
