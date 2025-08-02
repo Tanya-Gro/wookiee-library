afc55e7edcd765dea49976ba1a42355fb7b639bf
unknown <tarpel.gta@gmail.com>
2025-08-02 11:49:09 +0300

    feat: create useCheckListStore

---
+++ src/store/useCheckList.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+
+interface CheckListStore {
+  selectedIds: string[];
+  toggleId: (id: string) => void;
+}
+
+export const useCheckListStore = create<CheckListStore>((set) => ({
+  selectedIds: [],
+  toggleId: (id): void =>
+    set((state) => ({
+      selectedIds: state.selectedIds.includes(id)
+        ? state.selectedIds.filter((existingId) => existingId !== id)
+        : [...state.selectedIds, id],
+    })),
+}));
