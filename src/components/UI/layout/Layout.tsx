660543fc52ca3925ec9a3c221fb0326150ecff84
unknown <tarpel.gta@gmail.com>
2025-08-03 22:43:54 +0300

    feat: save theme in LS

--- src/components/UI/layout/Layout.tsx
+++ src/components/UI/layout/Layout.tsx
@@ -3,33 +3,43 @@ import type { FC } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import { useThemeStore } from '../../../store/useTheme';
 import styles from './Layout.module.css';
+import useLocalStorage from '../../../hooks/useLocalStorage';
 
 const Layout: FC = () => {
-  const lightTheme = useThemeStore((state) => state.isLight);
+  const isLightTheme = useThemeStore((state) => state.isLight);
   const toggleTheme = useThemeStore((state) => state.toggleTheme);
 
-  const theme = lightTheme ? 'light' : 'dark';
+  const [storedTheme, setStoredTheme] = useLocalStorage(
+    'isLightTheme',
+    isLightTheme
+  );
+
+  if (isLightTheme !== storedTheme) {
+    toggleTheme();
+  }
+
+  const theme = isLightTheme ? 'light' : 'dark';
   document.body.setAttribute('data-theme', theme);
 
   return (
     <>
       <header className="wrapper header">
         <nav>
-          <NavLink to="/home" className="link">
+          <NavLink to="/home" className={styles.link}>
             Home
           </NavLink>
-          <NavLink to="/about" className="link">
+          <NavLink to="/about" className={styles.link}>
             About
           </NavLink>
         </nav>
 
         <span
-          className={`material-symbols-outlined ${styles.theme_switch} ${lightTheme ? styles.light : styles.dark}`}
-          onClick={toggleTheme}
+          className={`material-symbols-outlined ${styles.theme_switch} ${isLightTheme ? styles.light : styles.dark}`}
+          onClick={() => setStoredTheme(!isLightTheme)}
           title="Toggle theme"
           role="button"
         >
-          {lightTheme ? 'light_mode' : 'dark_mode'}
+          {isLightTheme ? 'light_mode' : 'dark_mode'}
         </span>
       </header>
       <main className="wrapper grow column">
