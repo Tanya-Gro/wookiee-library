7de60ac8fcd7775a13bd379c253a109de061bde1
unknown <tarpel.gta@gmail.com>
2025-08-10 09:29:22 +0300

    style: move ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import './index.css';
 import App from './App.tsx';
-import ErrorBoundary from './components/ErrorBoundary.tsx';
+import ErrorBoundary from './components/errorBoundary/ErrorBoundary.tsx';
 import { ThemeProvider } from './context/ThemeProvider.tsx';
 
 const rootElement = document.getElementById('root');
