6fa3ca75cae1f0a8f20f3e3bde85a44c5ba1431e
unknown <tarpel.gta@gmail.com>
2025-08-17 18:45:43 +0300

    refactor: remove tests

--- src/test-utils/mocks/blob.ts
+++
@@ -1,4 +0,0 @@
-export const MOCK_BLOB =
-  'id,name,imageURL\n' +
-  '1,"Luke Skywalker","https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg"\n' +
-  '11,"Anakin Skywalker","https://vignette.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png"';
