116f607868faae3216a14da3ff0fe36a7dce84e6
unknown <tarpel.gta@gmail.com>
2025-08-25 01:33:09 +0300

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 import path from 'path';
+import react from '@vitejs/plugin-react';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
