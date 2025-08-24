b4d6e62c53e3f200877bb18b8bf453d1a2c761aa
unknown <tarpel.gta@gmail.com>
2025-08-24 23:30:31 +0300

    refactor: implement reatchers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
-import './App.css';
+import { useState } from 'react';
+import { useFormsStore } from '@/store/useFormsStore';
+import Card from './components/card/Card';
+import Modal from '@/components/modal/Modal';
+import HookForm from '@/components/forms/HookForm';
+import UncontrolledForm from '@/components/forms/UncontrolledForm';
+import '@/App.css';
 
 function App() {
-  return <></>;
+  const [isControlled, setIsControlled] = useState<boolean | null>(null);
+  const { forms } = useFormsStore();
+  const handleCloseModal = () => setIsControlled(null);
+  return (
+    <>
+      <header>
+        <button onClick={() => setIsControlled(false)}>
+          Uncontrolled Form
+        </button>
+        <button onClick={() => setIsControlled(true)}>Controlled Form</button>
+      </header>
+      <main>
+        <div className="card_wrapper">
+          {forms.map((form, i) => (
+            <Card data={form} key={i} />
+          ))}
+        </div>
+
+        {isControlled === false && (
+          <Modal onClose={handleCloseModal}>
+            <UncontrolledForm onSuccess={handleCloseModal} />
+          </Modal>
+        )}
+
+        {isControlled === true && (
+          <Modal onClose={handleCloseModal}>
+            <HookForm onSuccess={handleCloseModal} />
+          </Modal>
+        )}
+      </main>
+    </>
+  );
 }
 
 export default App;
